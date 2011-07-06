f294e8140ff405ef2dfef4da5ca924d220444e90
yama <yamamoto@kyms.ne.jp>
2011-07-06 15:35:01 +0900

    breadcrumb->trail()関数調整

--- includes/classes/breadcrumb.php
+++ includes/classes/breadcrumb.php
@@ -40,32 +40,42 @@ class breadcrumb extends base {
     $this->_trail[] = array('title' => $title, 'link' => $link);
   }
 
-  function trail($separator = '&nbsp;&nbsp;') {
-    $trail_string = '';
-
-    for ($i=0, $n=sizeof($this->_trail); $i<$n; $i++) {
-//    echo 'breadcrumb ' . $i . ' of ' . $n . ': ' . $this->_trail[$i]['title'] . '<br />';
-      $skip_link = false;
-		  if ($i==($n-1) && DISABLE_BREADCRUMB_LINKS_ON_LAST_ITEM =='true') {
-        $skip_link = true;
-      }
-      if (isset($this->_trail[$i]['link']) && zen_not_null($this->_trail[$i]['link']) && !$skip_link ) {
-        // this line simply sets the "Home" link to be the domain/url, not main_page=index?blahblah:
-        if ($this->_trail[$i]['title'] == HEADER_TITLE_CATALOG) {
-          $trail_string .= '  <a href="' . HTTP_SERVER . DIR_WS_CATALOG . '">' . $this->_trail[$i]['title'] . '</a>';
-        } else {
-          $trail_string .= '  <a href="' . $this->_trail[$i]['link'] . '">' . $this->_trail[$i]['title'] . '</a>';
-        }
-      } else {
-        $trail_string .= $this->_trail[$i]['title'];
-      }
-
-      if (($i+1) < $n) $trail_string .= $separator;
-      $trail_string .= "\n";
-    }
-
-    return $trail_string;
-  }
+function trail($separator = '&nbsp;&nbsp;')
+{
+	if(count($_GET)==1 && $_GET['main_page']=='index') return;
+	$trail_string = '';
+	
+	for ($i=0, $n=sizeof($this->_trail); $i<$n; $i++)
+	{
+		//    echo 'breadcrumb ' . $i . ' of ' . $n . ': ' . $this->_trail[$i]['title'] . '<br />';
+		$skip_link = false;
+		if ($i==($n-1) && DISABLE_BREADCRUMB_LINKS_ON_LAST_ITEM =='true')
+		{
+			$skip_link = true;
+		}
+		if (isset($this->_trail[$i]['link']) && zen_not_null($this->_trail[$i]['link']) && !$skip_link )
+		{
+			// this line simply sets the "Home" link to be the domain/url, not main_page=index?blahblah:
+			if ($this->_trail[$i]['title'] == HEADER_TITLE_CATALOG)
+			{
+				$trail_string .= '  <a href="' . HTTP_SERVER . DIR_WS_CATALOG . '">' . $this->_trail[$i]['title'] . '</a>';
+			}
+			else
+			{
+				$trail_string .= '  <a href="' . $this->_trail[$i]['link'] . '">' . $this->_trail[$i]['title'] . '</a>';
+			}
+		}
+		else
+		{
+			$trail_string .= $this->_trail[$i]['title'];
+		}
+		
+		if (($i+1) < $n) $trail_string .= $separator;
+		$trail_string .= "\n";
+	}
+	
+	return $trail_string;
+}
 
   function last() {
     $trail_size = sizeof($this->_trail);
